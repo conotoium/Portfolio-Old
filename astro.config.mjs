d123ee0c60671e8490c05edbfe56f8e4c92a5a3a
Contonium365 <87113453+conotoium@users.noreply.github.com>
2024-08-05 09:32:28 +0930

    change time

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://conotoium.github.io/Portfolio/',
@@ -10,5 +12,8 @@ export default defineConfig({
     checkOrigin: true
   },
   integrations: [tailwind(), react()],
-  output: "static"
+  output: "static",
+  adapter: node({
+    mode: "standalone"
+  })
 });
