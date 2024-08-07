3c8f105642ac5de9b6ce0e0ced792e90735db07d
Contonium365 <87113453+conotoium@users.noreply.github.com>
2024-08-07 12:00:26 +0930

    alrighty

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import node from "@astrojs/node";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +12,7 @@ export default defineConfig({
   security: {
     checkOrigin: true
   },
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), robotsTxt()],
   output: "static",
   adapter: node({
     mode: "standalone"
